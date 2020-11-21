bc3bec75e8b92a18a8481866826c5a793bcca359
manuuuskp <manojprabhuskp@gmail.com>
2020-11-21 10:06:24 +0530

    Card loading done

--- src/Pokedox.js
+++ src/Pokedox.js
@@ -1,6 +1,15 @@
-import React from "react";
-import { AppBar, Toolbar, Grid, Card, CardContent } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
+import data from "./data";
 
 const useStyles = makeStyles({
   gridContainer: {
@@ -10,28 +19,39 @@ const useStyles = makeStyles({
   }
 });
 
-const getCardData = () => {
-  return (
-    <Grid item xs={12} sm={4}>
-      <Card>
-        <CardContent>Hi</CardContent>
-      </Card>
-    </Grid>
-  );
+const getPokeName = (name) => {
+  return name.charAt(0).toUpperCase() + name.slice(1);
 };
 
 const Pokedox = () => {
+  const [pokedoxData, setPokedoxData] = useState(data);
   const classes = useStyles();
+
+  const getCardData = (pokeId) => {
+    const { id, name, sprites } = pokedoxData[pokeId];
+    console.log(sprites);
+    return (
+      <Grid item xs={12} md={4} sm={6} key={id}>
+        <Card style={{ height: "300px", width: "300px" }}>
+          <CardMedia
+            style={{ width: "130px", height: "130px" }}
+            image={sprites["front_default"]}
+          />
+          <CardContent>
+            <Typography>{`${id}.${getPokeName(name)}`}</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  };
+
   return (
     <>
       <AppBar position="static">
         <Toolbar>Hi</Toolbar>
       </AppBar>
       <Grid container spacing={2} className={classes.gridContainer}>
-        {getCardData()}
-        {getCardData()}
-        {getCardData()}
-        {getCardData()}
+        {Object.keys(pokedoxData).map((pokeId) => getCardData(pokeId))}
       </Grid>
     </>
   );
